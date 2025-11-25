d1e3d64e36cbe2d39f23ef9e515ee05bbbdd8954
atomi3973 <atomiroi3973@gmail.com>
2025-11-25 23:05:48 +0900

    corsデプロイ用設定追加

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000' # 開発環境Next.jsのURL
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins Rails.env.production? ? 'https://test-next-rails-app.vercel.app/' : 'http://localhost:3000'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end

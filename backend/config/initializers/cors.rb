230b40bf8f4a121bf90921183051b8f341e16068
atomi3973 <atomiroi3973@gmail.com>
2025-11-25 23:11:05 +0900

    cors設定修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.env.production? ? 'https://test-next-rails-app.vercel.app/' : 'http://localhost:3000'
+    origins Rails.env.production? ? 'https://test-next-rails-app.vercel.app' : 'http://localhost:3000'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
